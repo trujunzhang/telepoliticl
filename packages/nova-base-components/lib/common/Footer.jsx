901d2b912604997a57b7fef7481c8c3c5ba49184
trujunzhang <trujunzhang@gmail.com>
2016-09-01 19:46:14 +0800

    user's login.

--- packages/nova-base-components/lib/common/Footer.jsx
+++ packages/nova-base-components/lib/common/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 
 const Footer = props => {
-  return (
-    <div className="footer"><a href="http://telescopeapp.org" target="_blank"><FormattedMessage id="app.powered_by"/></a></div>
-  )
-}
+    return (
+      <footer id="colofon">
+          <section id="footer-bottom">
+
+              <div className="copyright-info">
+                  <span className="copyright">Copyright © 2016  Politicl.</span>
+                  <br/>
+                  <span className="author">
+					<p>Made with &lt;3 in India</p>
+				</span>
+              </div>
+          </section>
+      </footer>
+    )
+};
 
 Footer.displayName = "Footer";
 
