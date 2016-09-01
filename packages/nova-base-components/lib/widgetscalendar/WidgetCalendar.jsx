4f14648e42b5586d5f896e2fd10eb4a88f605bf8
trujunzhang <trujunzhang@gmail.com>
2016-09-01 14:04:12 +0800

    user's login.

--- packages/nova-base-components/lib/widgetscalendar/WidgetCalendar.jsx
+++ packages/nova-base-components/lib/widgetscalendar/WidgetCalendar.jsx
@@ -32,44 +32,41 @@ class WidgetCalendar extends Component {
 
     renderHeader() {
         return (
-          <div style="position:relative;" data-reactid="1273"><a class="rc-calendar-prev-year-btn" role="button"
-                                                                 title="Last year (Control + left)" data-reactid="1274">«</a><a
-            class="rc-calendar-prev-month-btn" role="button" title="Previous month (PageUp)"
-            data-reactid="1275">‹</a><span class="rc-calendar-my-select" data-reactid="1276"><a
-            class="rc-calendar-month-select" role="button" title="Choose a month" data-reactid="1277">August</a><a
-            class="rc-calendar-year-select" role="button" title="Choose a month" data-reactid="1278">2016</a></span><a
-            class="rc-calendar-next-month-btn" title="Next month (PageDown)" data-reactid="1279">›</a><a
-            class="rc-calendar-next-year-btn" title="Next year (Control + right)" data-reactid="1280">»</a></div>
+            <div className="calendar-header-container">
+                <a className="rc-calendar-prev-year-btn" role="button" title="Last year (Control + left)">«</a>
+                <a className="rc-calendar-prev-month-btn" role="button" title="Previous month (PageUp)">‹</a>
+                <span className="rc-calendar-my-select">
+                    <a className="rc-calendar-month-select" role="button" title="Choose a month">August</a>
+                    <a className="rc-calendar-year-select" role="button" title="Choose a month">2016</a>
+                </span>
+                <a className="rc-calendar-next-month-btn" title="Next month (PageDown)">›</a>
+                <a className="rc-calendar-next-year-btn" title="Next year (Control + right)">»</a>
+            </div>
         )
     }
 
     render() {
         return (
-          <div className="rc-calendar">
-              <div className="rc-calendar-date-panel">
-                  <div className="rc-calendar-header">
-                      {this.renderHeader()}
-                  </div>
-                  <Telescope.components.DayNames/>
-                  {this.renderWeeks()}
-              </div>
-          </div>
+            <div className="rc-calendar">
+                <div className="rc-calendar-date-panel">
+                    <div className="rc-calendar-header">
+                        {this.renderHeader()}
+                    </div>
+                    <Telescope.components.DayNames/> {this.renderWeeks()}
+                </div>
+            </div>
         )
     }
 
     renderWeeks() {
         var weeks = [],
-          done = false,
-          date = this.state.month.clone().startOf("month").add("w" - 1).day("Sunday"),
-          monthIndex = date.month(),
-          count = 0;
+            done = false,
+            date = this.state.month.clone().startOf("month").add("w" - 1).day("Sunday"),
+            monthIndex = date.month(),
+            count = 0;
 
         while (!done) {
-            weeks.push(<Telescope.components.Week key={date.toString()}
-                                                  date={date.clone()}
-                                                  month={this.state.month}
-                                                  select={this.select}
-                                                  selected={this.props.selected}/>);
+            weeks.push(<Telescope.components.Week key={date.toString()} date={date.clone()} month={this.state.month} select={this.select} selected={this.props.selected}/>);
             date.add(1, "w");
             done = count++ > 2 && monthIndex !== date.month();
             monthIndex = date.month();
@@ -79,7 +76,9 @@ class WidgetCalendar extends Component {
     }
 
     renderMonthLabel() {
-        return (<span>{this.state.month.format("MMMM, YYYY")}</span>)
+        return (
+            <span>{this.state.month.format("MMMM, YYYY")}</span>
+        )
     }
 
 }
@@ -91,4 +90,3 @@ WidgetCalendar.contextTypes = {
 WidgetCalendar.displayName = "WidgetCalendar";
 
 module.exports = WidgetCalendar;
-    
