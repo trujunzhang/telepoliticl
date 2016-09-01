e76b83c0ba53f0c07946af18e422ae26616e793d
trujunzhang <trujunzhang@gmail.com>
2016-09-01 19:39:45 +0800

    user's login.

--- packages/nova-base-components/lib/posts/PostsLoadMore.jsx
+++ packages/nova-base-components/lib/posts/PostsLoadMore.jsx
@@ -3,11 +3,12 @@ import {FormattedMessage} from 'react-intl';
 
 const PostsLoadMore = ({loadMore, count, totalCount}) => {
     return (
-      <a className="posts-load-more" onClick={loadMore}>
-          <span><FormattedMessage id="posts.load_more"/></span>
-          &nbsp;
-          {totalCount ? <span className="load-more-count">{`(${count}/${totalCount})`}</span> : null}
-      </a>
+      <a className="posts-load-more" onClick={loadMore}>Show More…</a>
+      //<a className="posts-load-more" >
+      //    <span><FormattedMessage id="posts.load_more"/></span>
+      //    &nbsp;
+      //    {totalCount ? <span className="load-more-count">{`(${count}/${totalCount})`}</span> : null}
+      //</a>
     )
 };
 
