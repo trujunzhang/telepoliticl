c7fe0dd3a076006ed0c705f4028419b0ddd5a587
trujunzhang <trujunzhang@gmail.com>
2016-09-01 18:54:20 +0800

    user's login.

--- packages/nova-base-components/lib/widgettopics/WidgetTopics.jsx
+++ packages/nova-base-components/lib/widgettopics/WidgetTopics.jsx
@@ -12,52 +12,40 @@ class WidgetTopics extends Component {
     }
 
     render() {
+        const topics = [
+            {href: "/?cat=color-theory", title: ""},
+            {href: "/?cat=color-theory", title: "Aam Aadmi Party"},
+            {href: "/?cat=color-theory", title: "AAP in Punjab"},
+            {href: "/?cat=color-theory", title: "Punjab polls"},
+            {href: "/?cat=color-theory", title: "Sting videos"},
+            {href: "/?cat=color-theory", title: "Sucha Singh Chhotepur"},
+            {href: "/?cat=color-theory", title: "Deepa Seetharam"},
+            {href: "/?cat=color-theory", title: "Foreign Service"},
+            {href: "/?cat=color-theory", title: "IFS"},
+            {href: "/?cat=color-theory", title: "UAE"},
+            {href: "/?cat=color-theory", title: "United Arab Emirates"},
+            {href: "/?cat=color-theory", title: "Bangla"},
+            {href: "/?cat=color-theory", title: "Mamata Banerjee"},
+            {href: "/?cat=color-theory", title: "West Bengal"},
+            {href: "/?cat=color-theory", title: "Advertising"},
+            {href: "/?cat=color-theory", title: "facebook"},
+            {href: "/?cat=color-theory", title: "Tech"},
+            {href: "/?cat=color-theory", title: "WhatsApp privacy policy"},
+            {href: "/?cat=color-theory", title: "WhatsApp-Facebook data sharing"},
+        ];
         return (
-          <div className="paddedBox_2UY-S box_c4OJj sidebarBox_1-7Yk sidebarBoxPadding_y0KxM">
-              <div className="content_DcBqe">
-                  <div className="rc-calendar">
-                      <div className="rc-calendar-date-panel">
-                          <div className="rc-calendar-header">
-                              {this.renderHeader()}
-                          </div>
-                          <div className="rc-calendar-calendar-body">
-                              <table className="rc-calendar-table" cellspacing="0" role="grid">
-                                  <thead >
-                                  {this.renderDayNames()}
-                                  </thead>
-                                  <tbody className="rc-calendartbody">
-                                  {this.renderWeeks()}
-                                  </tbody>
-                              </table>
-                          </div>
-                      </div>
-                  </div>
-              </div>
+          <div className="sidebar-block sidebar-categories">
+              <h4 className="sidebar-heading">Categories</h4>
+              <ul className="categories-list">
+                  {topics.map((item, key) => {
+                      return (
+                        <li className="category-menu-item">
+                            <a className="posts-category" href={item.href}>{item.title}</a>
+                        </li>
+                      )
+                  })}
+              </ul>
           </div>
-        )
-    }
-
-    renderWeeks() {
-        var weeks = [],
-          done = false,
-          date = this.state.month.clone().startOf("month").add("w" - 1).day("Sunday"),
-          monthIndex = date.month(),
-          count = 0;
-
-        while (!done) {
-            weeks.push(<Telescope.components.Week key={date.toString()} date={date.clone()} month={this.state.month}
-                                                  select={this.select.bind(this)} selected={this.state.month}/>);
-            date.add(1, "w");
-            done = count++ > 2 && monthIndex !== date.month();
-            monthIndex = date.month();
-        }
-
-        return weeks;
-    }
-
-    renderMonthLabel() {
-        return (
-          <span>{this.state.month.format("MMMM, YYYY")}</span>
         )
     }
 
